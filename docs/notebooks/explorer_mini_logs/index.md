d8b43f7ed11f12b418194cd6feea93fccfd31260
Yishuang Chen <yishuangchen17@gmail.com>
2020-06-16 18:14:48 +0000

    update monthly collection

--- docs/notebooks/explorer_mini_logs/index.md
+++ docs/notebooks/explorer_mini_logs/index.md
@@ -2,6 +2,20 @@
 This page provides daily analysis for Mainnet.
 
 ## Latest Reports
+- [2020/06/01](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_01.html)
+- [2020/06/02](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_02.html)
+- [2020/06/03](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_03.html)
+- [2020/06/04](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_04.html)
+- [2020/06/05](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_05.html)
+- [2020/06/06](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_06.html)
+- [2020/06/07](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_07.html)
+- [2020/06/08](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_08.html)
+- [2020/06/09](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_09.html)
+- [2020/06/10](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_10.html)
+- [2020/06/11](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_11.html)
+- [2020/06/12](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_12.html)
+- [2020/06/13](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_13.html)
+- [2020/06/14](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_14.html)
 - [2020/06/15](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_2020_06_15.html)
 
-###[Old Reports](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/old_report)
+### [Old Reports](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/old_report)
--- projects/explorer_mini_log_analysis/daily_github.sh
+++ projects/explorer_mini_log_analysis/daily_github.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 head -n -2 /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md > /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/temp.md ; mv /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/temp.md /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md
-echo -e "- [`date -d 'yesterday 13:00' '+%Y/%m/%d'`](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_`date -d 'yesterday 13:00' '+%Y_%m_%d'`.html)\n\n### [Old Reports](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/old_report)" >> /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md
+VAR1=`date -d 'yesterday 13:00' '+%Y/%m'`
+VAR2=`date '+%Y/%m'`
+num=`date -d 'yesterday 13:00' '+%d'`
+month=`date -d 'yesterday 13:00' '+%B'`
+if [ "$VAR1" != "$VAR2" ]; then
+	head -n 4 /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md > /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/temp.md ; 
+        echo -e "\n\n## $month" >> /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/old_report/index.md
+	echo -e "$(cat /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md | head -$(( 4+num )) | tail -$num)" >> /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/old_report/index.md ;
+      	mv /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/temp.md /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md 
+        echo -e "- [`date -d 'yesterday 13:00' '+%Y/%m/%d'`](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_`date -d 'yesterday 13:00' '+%Y_%m_%d'`.html)\n\n###[Old Reports](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/old_report)" >> /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md
+else
+       	echo -e "- [`date -d 'yesterday 13:00' '+%Y/%m/%d'`](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/mainnet_`date -d 'yesterday 13:00' '+%Y_%m_%d'`.html)\n\n### [Old Reports](https://harmony-one.github.io/harmony-log-analysis/notebooks/explorer_mini_logs/old_report)" >> /home/ubuntu/jupyter/harmony-log-analysis/docs/notebooks/explorer_mini_logs/index.md
+fi
 
-cd /home/ubuntu/jupyter/harmony-log-analysis && git add docs/graphs/test_logs/ && git add docs/notebooks/explorer_mini_logs/ && git add graphs/test_logs/
-git commit -m "daily cronjob push graphs "`date +"%Y_%m_%d"`
-git push origin master
+#cd /home/ubuntu/jupyter/harmony-log-analysis && git add docs/graphs/test_logs/ && git add docs/notebooks/explorer_mini_logs/ && git add graphs/test_logs/
+#git commit -m "daily cronjob push graphs "`date +"%Y_%m_%d"`
+#git push origin master
